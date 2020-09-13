5fd43f7f6cc9e1fac8f7189c3252d7a326636576
Saiful Islam <saifulislampp23@gmail.com>
2020-09-13 16:53:39 +0600

    finished

--- lib/earthquake_screen.dart
+++ lib/earthquake_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'model/earthquakes.dart';
 import 'package:google_maps/network/network.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -15,6 +15,7 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
   Future<Earthquake> data;
   Completer<GoogleMapController> _controller = Completer();
   List<Marker> _markerList = <Marker>[];
+  double _zoomVal = 5.0;
 
   @override
   void initState() {
@@ -24,32 +25,26 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
   }
 
   void _handleResponse() {
-    setState(
-      () {
-        data.then(
-          (earthquake) => {
-            earthquake.features.forEach(
-              (earthquake) => {
-                _markerList.add(
-                  Marker(
-                    markerId: MarkerId(earthquake.id),
-                    infoWindow: InfoWindow(
-                      title: earthquake.properties.mag.toString(),
-                      snippet: earthquake.properties.title,
+    setState(() {
+      data.then((earthquake) => {
+            earthquake.features.forEach((earthquake) => {
+                  _markerList.add(
+                    Marker(
+                      markerId: MarkerId(earthquake.id),
+                      infoWindow: InfoWindow(
+                        title: earthquake.properties.mag.toString(),
+                        snippet: earthquake.properties.title,
+                      ),
+                      icon: BitmapDescriptor.defaultMarkerWithHue(
+                          BitmapDescriptor.hueMagenta),
+                      position: LatLng(earthquake.geometry.coordinates[1],
+                          earthquake.geometry.coordinates[0]),
+                      onTap: () {},
                     ),
-                    icon: BitmapDescriptor.defaultMarkerWithHue(
-                        BitmapDescriptor.hueMagenta),
-                    position: LatLng(earthquake.geometry.coordinates[1],
-                        earthquake.geometry.coordinates[0]),
-                    onTap: () {},
                   ),
-                ),
-              },
-            ),
-          },
-        );
-      },
-    );
+                }),
+          });
+    });
   }
 
   void findEarthquake() {
@@ -59,12 +54,45 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
     });
   }
 
+  Future<void> _zoomIn(double _zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(40.712776, -74.005974), zoom: _zoomVal),
+      ),
+    );
+  }
+
+  Future<void> _zoomOut(double _zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(40.712776, -74.005974), zoom: _zoomVal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           _buildShowMapScreen(context),
+          Positioned(
+            left: 20,
+            top: 38,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                  color: Colors.blue),
+              child: Row(
+                children: [
+                  _zoomPlus(),
+                  _zoomMinus(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -81,13 +109,45 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       child: GoogleMap(
-        mapType: MapType.normal,
+        mapType: MapType.terrain,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
         initialCameraPosition:
             CameraPosition(target: LatLng(36.1083333, -117.86083333), zoom: 3),
         markers: Set<Marker>.of(_markerList),
+      ),
+    );
+  }
+
+  Widget _zoomPlus() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+        onPressed: () {
+          _zoomVal++;
+          _zoomIn(_zoomVal);
+        },
+        icon: Icon(
+          FontAwesomeIcons.searchPlus,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget _zoomMinus() {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+        onPressed: () {
+          _zoomVal--;
+          _zoomOut(_zoomVal);
+        },
+        icon: Icon(
+          FontAwesomeIcons.searchMinus,
+          color: Colors.white,
+        ),
       ),
     );
   }
