df9f92924101e6a235e8102de1011bbecc517eee
Saiful Islam <saifulislampp23@gmail.com>
2020-09-14 21:17:18 +0600

    final task

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: EarthquakeScreen.id,
       routes: {
         ShowGoogleMap.id: (_) => ShowGoogleMap(),
